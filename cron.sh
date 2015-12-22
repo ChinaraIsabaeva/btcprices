53154ba25c33560cc1240efa9145a8f081441ba1
chinara <chinara.isabaeva@gmail.com>
2015-12-22 18:48:18 +0000

    remove local cron file

--- cron.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-export WORKON_HOME=/home/chinara/Envs
-source /usr/local/bin/virtualenvwrapper.sh
-workon btc-trading
-cd /home/chinara/dev/my_projects/btc-trading/lib
-python get_prices.py
