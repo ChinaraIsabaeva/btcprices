462d6909093c9340afd16f3548f21f82565ecfc9
chinara <chinara.isabaeva@gmail.com>
2016-02-28 12:14:01 +0000

    update cron_live with renamed dir

--- cron_live.sh
+++ cron_live.sh
@@ -4,6 +4,6 @@ export WORKON_HOME=$HOME/.virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
 
 workon btcprices
-cd /var/www/btcprices/lib
+cd /var/www/btcprices.info/lib
 python get_prices.py
 
